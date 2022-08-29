1d75a6ceaa9730d70786e42bdd9999db373250c6
ajspryn <adjisupriyono@gmail.com>
2022-08-29 11:37:32 +0700

    timeline permaruan ao

--- Modules/Skpd/Resources/views/komite/lihat.blade.php
+++ Modules/Skpd/Resources/views/komite/lihat.blade.php
@@ -931,6 +931,8 @@
                                                                                 {{ $timeline->statushistory->keterangan }}
                                                                                 {{ $timeline->jabatan->keterangan }}
                                                                             </h6>
+                                                                            <span
+                                                                                class="timeline-event-time">{{ $timeline->created_at->isoformat('dddd, D MMMM Y') }}</span>
                                                                         </div>
                                                                         @if ($timeline->catatan)
                                                                             <p value="{{ $timeline->id }}"> <br>Catatan :
