05595894bd56558f048dcd0121c57b8b3051e438
ajspryn <adjisupriyono@gmail.com>
2022-08-31 15:16:24 +0700

    perbaikan model skpd

--- Modules/Skpd/Entities/SkpdPembiayaan.php
+++ Modules/Skpd/Entities/SkpdPembiayaan.php
@@ -23,57 +23,55 @@ class SkpdPembiayaan extends Model
 
     public function nasabah()
     {
-        return $this->belongsTo(SkpdNasabah::class,'skpd_nasabah_id', 'id');
+        return $this->belongsTo(SkpdNasabah::class, 'skpd_nasabah_id', 'id');
     }
 
     public function instansi()
     {
-        return $this->belongsToMany(SkpdInstansi::class,'skpd_instansi_id', 'id');
+        return $this->belongsTo(SkpdInstansi::class, 'skpd_instansi_id', 'id');
     }
 
     public function golongan()
     {
-        return $this->belongsTo(SkpdGolongan::class,'skpd_golongan_id', 'id');
+        return $this->belongsTo(SkpdGolongan::class, 'skpd_golongan_id', 'id');
     }
 
     public function jenis_penggunaan()
     {
-        return $this->belongsTo(SkpdJenisPenggunaan::class,'skpd_jenis_penggunaan_id', 'id');
+        return $this->belongsTo(SkpdJenisPenggunaan::class, 'skpd_jenis_penggunaan_id', 'id');
     }
 
     public function status_perkawinan()
     {
-        return $this->belongsTo(SkpdStatusPerkawinan::class,'skpd_status_perkawinan_id', 'id');
+        return $this->belongsTo(SkpdStatusPerkawinan::class, 'skpd_status_perkawinan_id', 'id');
     }
 
     public function sektor()
     {
-        return $this->belongsTo(SkpdSektorEkonomi::class,'skpd_sektor_ekonomi_id', 'id');
+        return $this->belongsTo(SkpdSektorEkonomi::class, 'skpd_sektor_ekonomi_id', 'id');
     }
 
     public function akad()
     {
-        return $this->belongsTo(SkpdAkad::class,'skpd_akad_id', 'id');
+        return $this->belongsTo(SkpdAkad::class, 'skpd_akad_id', 'id');
     }
 
     public function jaminan()
     {
-        return $this->belongsTo(SkpdJaminan::class,'id', 'skpd_pembiayaan_id');
+        return $this->belongsTo(SkpdJaminan::class, 'id', 'skpd_pembiayaan_id');
     }
     public function deviasi()
     {
-        return $this->belongsTo(SkpdDeviasi::class,'id', 'skpd_pembiayaan_id');
+        return $this->belongsTo(SkpdDeviasi::class, 'id', 'skpd_pembiayaan_id');
     }
 
     public function foto()
     {
-        return $this->belongsTo(SkpdFoto::class,'id', 'skpd_pembiayaan_id');
+        return $this->belongsTo(SkpdFoto::class, 'id', 'skpd_pembiayaan_id');
     }
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
-
 }
