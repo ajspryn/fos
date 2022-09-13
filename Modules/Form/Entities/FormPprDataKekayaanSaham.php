877b0fd1ba8af73bd30d9d5d18dcf3d53b6b318f
Adrie <adriefahriza3@gmail.com>
2022-09-13 16:47:17 +0700

    optimasi form proposal ppr

--- Modules/Form/Entities/FormPprDataKekayaanSaham.php
+++ Modules/Form/Entities/FormPprDataKekayaanSaham.php
@@ -4,20 +4,26 @@ namespace Modules\Form\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Modules\Form\Entities\FormPprPembiayaan;
+use Modules\Form\Entities\FormPprPembiayaan;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FormPprDataKekayaanSaham extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = [
         'created_at'
     ];
 
-    // public function pembiayaan()
-    // {
-    //     return $this->belongsTo(FormPprPembiayaan::class, 'form_ppr_pembiayaan_id', 'id');
-    // }
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    public function pembiayaan()
+    {
+        return $this->belongsTo(FormPprPembiayaan::class, 'form_ppr_pembiayaan_id', 'id');
+    }
 
     protected static function newFactory()
     {
