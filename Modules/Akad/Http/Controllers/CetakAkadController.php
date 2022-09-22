97250e3e37ae06c0768db571323d1722549da271
devifatmawati754@gmail.com <devifatmawati743@gmail.com>
2022-09-22 11:17:07 +0700

    perubahan akad

--- Modules/Akad/Http/Controllers/CetakAkadController.php
+++ Modules/Akad/Http/Controllers/CetakAkadController.php
@@ -15,7 +15,7 @@ class CetakAkadController extends Controller
      */
     public function index()
     {
-        return view('akad::ijarah.pasar',
+        return view('akad::murabahah.pasar',
         [
             'title'=>'Cetak Akad',
             'now'=>Carbon::now()->isoFormat('D MMMM Y'),
